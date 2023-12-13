1c5990d042a438e0732829885c831fb0a2868641
pramodkulkarniRK <98514441+pramodkulkarniRK@users.noreply.github.com>
2023-12-13 15:53:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "us-east-1"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+resource "aws_instance" "example" {
+  ami                     = "ami-0fc5d935ebf8bc3bc"
+  instance_type           = "t2.micro"
 }
